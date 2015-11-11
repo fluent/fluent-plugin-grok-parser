9f9967c1c6a5b40f19064b3cb82db51edaf781dd
Kenji Okimoto <okimoto@clear-code.com>
2015-11-11 17:01:19 +0900

    Add missing helper.rb stolen from fluentd's test

---
+++ test/helper.rb
@@ -0,0 +1,18 @@
+
+def unused_port
+  s = TCPServer.open(0)
+  port = s.addr[1]
+  s.close
+  port
+end
+
+def ipv6_enabled?
+  require 'socket'
+
+  begin
+    TCPServer.open("::1", 0)
+    true
+  rescue
+    false
+  end
+end
