8b7a55a5bcd4903b80da7c0de6293160742e21c3
Kenji Okimoto <okimoto@clear-code.com>
2015-11-16 17:24:35 +0900

    Add create_parser for parser test

--- test/test_multiline_grok_parser.rb
+++ test/test_multiline_grok_parser.rb
@@ -1,4 +1,7 @@
 require 'fluent/plugin/parser_multiline_grok'
+require 'fluent/config/parser'
+
+require 'stringio'
 
 class MultilineGrokParserTest < Test::Unit::TestCase
   def test_multiline
@@ -12,15 +15,22 @@ message1
  message2
  message3
 MESSAGE
-    parser = TextParser::MultilineGrokParser.new
-    options = {
-      "grok_pattern" => '%{HOSTNAME:hostname} %{GREEDYDATA:message}',
-      "multiline_start_regexp" => '/^\s/'
-    }
-    parser.configure(Config::Element.new('ROOT', '', options, []))
+    conf = %[
+      grok_pattern %{HOSTNAME:hostname} %{GREEDYDATA:message}
+      multiline_start_regexp /^\s/
+    ]
+    parser = create_parser(conf)
 
     parser.parse(text) do |time, record|
       assert_equal({ "hostname" => "host1", "message" => message }, record)
     end
+  end
+  private
+
+  def create_parser(conf)
+    parser = TextParser::MultilineGrokParser.new
+    io = StringIO.new(conf)
+    parser.configure(Config::Parser.parse(io, "fluent.conf"))
+    parser
   end
 end
