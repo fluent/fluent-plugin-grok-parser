6fce20572bdc2bec55ad5be8dd98fafcdbd5555a
Kenji Okimoto <okimoto@clear-code.com>
2015-12-29 10:27:34 +0900

    test: Rename

--- test/test_grok_parser.rb
+++ test/test_grok_parser.rb
@@ -82,7 +82,7 @@ class GrokParserTest < ::Test::Unit::TestCase
   end
 
   class OptionalType < self
-    def test_optional_type
+    def test_simple
       internal_test_grok_pattern('%{INT:user_id:integer} paid %{NUMBER:paid_amount:float}',
                                  '12345 paid 6789.10', nil,
                                  {"user_id" => 12345, "paid_amount" => 6789.1 })
